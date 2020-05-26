5f769c81152c8d36d694ebc304bec448a8ddd191
jaredaguirre <64754731+jaredaguirre@users.noreply.github.com>
2020-05-25 22:28:01 -0300

    Add files via upload

---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+#include "C:\Users\54112\Desktop\Ejercicios en C\dinamica\pila.h"
+
+void mostrarPila(tPila*);
+void sumarPilas(tPila*, tPila*, tPila*);
+#endif // MAIN_H_INCLUDED
